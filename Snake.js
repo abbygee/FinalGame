e8b8058bd98bcee66efdb937a9946e809bda03cb
abbygee <abigaildelgado@students.berkeley.net>
2019-06-04 14:25:32 -0700

    progress yum

--- Snake.js
+++ Snake.js
@@ -1,5 +1,63 @@
 class Snake {
-    constructor(){
-
+    constructor() {
+        this.body = [];
+        this.body[0] = new component(20, 20, "blue", 300, 300);
+        this.xdir = 0;
+        this.ydir = 0;
+        //this.len = 0;
+        //this.speed = 1
     }
+
+    // setDir(x, y) {
+    //     this.xdir = x;
+    //     this.ydir = y;
+    // }
+    //
+    // update() {
+    //     var head = this.body[this.body.length-1].copy();
+    //     this.body.shift();
+    //     head[0] += this.xdir;
+    //     head[1] += this.ydir;
+    //     this.body.push(head);
+    // }
+    //
+    // grow() {
+    //     var head = this.body[this.body.length-1].copy();
+    //     this.len++;
+    //     this.body.push(head);
+    // }
+    //
+    // endGame() {
+    //     var x = this.body[this.body.length-1][0];
+    //     var y = this.body[this.body.length-1][1];
+    //     if(x > w-1 || x < 0 || y > h-1 || y < 0) {
+    //         return true;
+    //     }
+    //     for(var i = 0; i < this.body.length-1; i++) {
+    //         var part = this.body[i];
+    //         if(part[0] === x && part[1] === y) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+    //
+    // eat(pos) {
+    //     var x = this.body[this.body.length-1][0];
+    //     var y = this.body[this.body.length-1][1];
+    //     if(x === pos[0] && y === pos[1]) {
+    //         this.grow();
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    //
+    // show() {
+    //     for(var i = 0; i < this.body.length; i++) {
+    //         fill(0);
+    //         noStroke();
+    //         rect(this.body[i][0], this.body[i][1], 1, 1)
+    //     }
+    // }
+
 }
--- script.js
+++ script.js
@@ -1,19 +1,141 @@
-let s;
 
-function start(){
-    const can = document.getElementById("board");
-    const c = can.getContext("2d");
-    c.beginPath();
-    c.fillStyle = "lavender";
-    c.fillRect(0,0,can.width,can.height);
+// var food;
+// var lastKey;
+// var can;
 
-    s = new Snake();
+let food;
+let snake;
+let scale = 20;
+let w = 600;
+let h = 600;
+
+//features to implement: score?, can't go back on itself, end screen
+
+function setup() {
+    myGameArea.start();
+
+    food = new component(20, 20, "red", foodLocateX(), foodLocateY());
+    console.log(food);
+
+    snake = new Snake();
+    console.log(snake);
+    console.log(snake.body[0].x);
 }
 
-function key(e){
-    e = e || window.event;
+let myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = w;
+        this.canvas.height = h;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 100);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
 
-    switch(e.keyCode) {
-        case '38'
+function component(width, height, color, x, y)  {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+
+function foodLocateX(){
+    var cols = Math.floor(w/scale);
+
+    return Math.floor(Math.random()*cols) * scale;
+}
+
+function foodLocateY(){
+    var rows = Math.floor(h/scale);
+
+    return Math.floor(Math.random()*rows) * scale;
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    food.update();
+    snake.body[0].x += 1;
+    snake.body[0].update();
+}
+
+// function setup() {
+//     // can = b.getContext("2d");
+//     // can.fillStyle = "#d3d3d3";
+//     // can.fillRect(120, 0, 600, 600);
+//     // document.body.appendChild(b);
+//     draw();
+//     w = Math.floor(400 / ratio);
+//     h = Math.floor(400 / ratio);
+//     //frameRate(10);
+//     snake = new Snake();
+//     foodLocation();
+// }
+
+// function foodLocation() {
+//     var x = Math.floor(Math.random(w));
+//     var y = Math.floor(Math.random(h));
+//     food = [x, y];
+//
+// }
+//
+// function keyPressed() {
+//     switch(e.keyCode) {
+//         //up arrow
+//         case '38':
+//             if(!lastKey === '40'){
+//                 snake.setDir(0, -1);
+//                 lastKey = '38';
+//             }
+//             break;
+//         //down arrow
+//         case '40':
+//             if(!lastKey === '38'){
+//                 snake.setDir(0, 1);
+//                 lastKey = '40';
+//             }
+//             break;
+//         //right arrow
+//         case '39':
+//             if(!lastKey === '37'){
+//                 snake.setDir(1, 0);
+//                 lastKey = '39';
+//             }
+//             break;
+//         //left arrow
+//         case '37':
+//             if(!lastKey === '39'){
+//                 snake.setDir(-1, 0);
+//                 lastKey = '37';
+//             }
+//     }
+// }
+//
+// function draw() {
+//     //scale(ratio);
+//
+//     if (snake.eat(food)) {
+//         foodLocation();
+//     }
+//
+//     snake.update();
+//     snake.show();
+//
+//     if (snake.endGame()) {
+//         print("END GAME");
+//         //background(255, 0, 0);
+//         //noLoop();
+//     }
+//
+//     //noStroke();
+//     //fill(255, 0, 0);
+//     var f = new component(food[0], food[1], 1, 1);
+// }
