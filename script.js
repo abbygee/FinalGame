8c807b9857fd5b3a61d20d6465989ba9d0ac564f
abbygee <abigaildelgado@students.berkeley.net>
2019-06-06 14:27:52 -0700

    ummm

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-const GAME_SPEED = 100;
-const CANVAS_BORDER_COLOUR = 'black';
-const CANVAS_BACKGROUND_COLOUR = "white";
-const SNAKE_COLOUR = 'blue';
-const SNAKE_BORDER_COLOUR = 'white';
-const FOOD_COLOUR = 'red';
-const FOOD_BORDER_COLOUR = 'darkred';
-let snake = [
+var GAME_SPEED = 100;
+var CANVAS_BACKGROUND_COLOUR = "white";
+var snake = [
     {x: 150, y: 150},
     {x: 140, y: 150},
     {x: 130, y: 150},
     {x: 120, y: 150},
     {x: 110, y: 150}
 ];
-let score = 0;
-let changingDirection = false;
-let foodX;
-let foodY;
-let dx = 10;
-let dy = 0;
+var score = 0;
+var changingDirection = false;
+var foodX;
+var foodY;
+var dx = 10;
+var dy = 0;
 
 
-let gameCanvas;
-let ctx;
+var gameCanvas;
+var ctx;
 
 function main() {
     gameCanvas = document.getElementById("game");
@@ -42,24 +37,21 @@ function main() {
 
 function clearCanvas() {
     ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
-    ctx.strokestyle = CANVAS_BORDER_COLOUR;
     ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
-    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
 
 function drawFood() {
-    ctx.fillStyle = FOOD_COLOUR;
-    ctx.strokestyle = FOOD_BORDER_COLOUR;
+    ctx.fillStyle = 'red';
     ctx.fillRect(foodX, foodY, 10, 10);
     ctx.strokeRect(foodX, foodY, 10, 10);
 }
 
 function advanceSnake() {
     // Create the new Snake's head
-    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    var head = {x: snake[0].x + dx, y: snake[0].y + dy};
     // Add the new head to the beginning of snake body
     snake.unshift(head);
-    const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+    var didEatFood = snake[0].x === foodX && snake[0].y === foodY;
     if (didEatFood) {
         score += 1;
         document.getElementById('score').innerHTML = score;
@@ -71,13 +63,13 @@ function advanceSnake() {
 }
 
 function didGameEnd() {
-    for (let i = 4; i < snake.length; i++) {
+    for (var i = 4; i < snake.length; i++) {
         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
     }
-    const hitLeftWall = snake[0].x < 0;
-    const hitRightWall = snake[0].x > gameCanvas.width - 10;
-    const hitToptWall = snake[0].y < 0;
-    const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+    var hitLeftWall = snake[0].x < 0;
+    var hitRightWall = snake[0].x > gameCanvas.width - 10;
+    var hitToptWall = snake[0].y < 0;
+    var hitBottomWall = snake[0].y > gameCanvas.height - 10;
     return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall
 }
 
@@ -95,31 +87,35 @@ function createFood() {
 }
 
 function drawSnake() {
-    // loop through the snake parts drawing each part on the canvas
     snake.forEach(drawSnakePart)
 }
 
 function drawSnakePart(snakePart) {
-    ctx.fillStyle = SNAKE_COLOUR;
-    ctx.strokestyle = SNAKE_BORDER_COLOUR;
+    var gradient = ctx.createLinearGradient(0, 0, 400, 0);
+    gradient.addColorStop(0, "magenta");
+    gradient.addColorStop(0.5, "#89a0ed");
+    gradient.addColorStop(1.0, "#ff3f8c");
+
+    ctx.fillStyle = gradient;
+    ctx.strokestyle = "black";
 
     ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
 function changeDirection(event) {
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
-    const UP_KEY = 38;
-    const DOWN_KEY = 40;
+    var LEFT_KEY = 37;
+    var RIGHT_KEY = 39;
+    var UP_KEY = 38;
+    var DOWN_KEY = 40;
 
     if (changingDirection) return;
     changingDirection = true;
-    const keyPressed = event.keyCode;
-    const goingUp = dy === -10;
-    const goingDown = dy === 10;
-    const goingRight = dx === 10;
-    const goingLeft = dx === -10;
+    var keyPressed = event.keyCode;
+    var goingUp = dy === -10;
+    var goingDown = dy === 10;
+    var goingRight = dx === 10;
+    var goingLeft = dx === -10;
     if (keyPressed === LEFT_KEY && !goingRight) {
         dx = -10;
         dy = 0;
@@ -137,3 +133,5 @@ function changeDirection(event) {
         dy = 10;
     }
 }
+
+//oops
